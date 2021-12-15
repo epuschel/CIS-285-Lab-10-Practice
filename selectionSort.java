3cd35bd1258a129a5921de2629107b8538563930
epuschel <73982403+epuschel@users.noreply.github.com>
2021-12-14 21:18:48 -0500

    Set j equal to 0

--- selectionSort.java
+++ selectionSort.java
@@ -4,17 +4,17 @@ public class SelectionSort {
 	/** Creates a new instance of SelectionSort */
     public SelectionSort() {
     }
-    
+
     /* A simple SelectionSort algorithm
      * pre-condition: 
      * post-condition:
      * inputs:
      * outputs:
      * special conditions:
-     */    
+     */
     public int[] basicSelectionSort(int[] x) {
         for (int i = 0; i < x.length; ++i) {
-            for (int j= i+1; j < x.length; ++j) {
+            for (int j= 0; j < x.length; ++j) {
                 if (x[i] > x[j]) {
                     temp = x[i];
                     x[i] = x[j];
